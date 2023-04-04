0c46150ea0f536b89026fbfc4ac1dfb050e931aa
Feras Jobeir <fjobeir@gmail.com>
2023-04-04 18:13:31 +0300

    Correct answers

--- js/04-test/questions.js
+++ js/04-test/questions.js
@@ -61,3 +61,28 @@ for (var i = 0; i < questions.length; i++) {
 
     slider.innerHTML += newSlide
 }
+
+const resultBtn = document.getElementById('result-btn')
+resultBtn.addEventListener('click', function() {
+    // loop on the questions
+    // for every question, we will get the answers
+    // get the user answer
+    // compare it to the correct answer
+    // increase the final mark (if correct)
+    let finalResult = 0;
+    for (var i = 0; i < questions.length; i++) {
+        const answers = Array.from(document.getElementsByName(`q${i}`));
+        // const selectedAnswer = answers.filter((input) => input.checked)
+        const selectedAnswer = answers.find((input) => input.checked)
+        if (selectedAnswer) {
+            // This means that the user has answered this question
+            // but we still do not know if it is the correct answer or not
+            const answerIndex = selectedAnswer.value
+            if (questions[i].answers[answerIndex].isCorrect) {
+                // the user has answered correctly
+                finalResult += 1
+            }
+        }
+    }
+    window.alert(`You had ${finalResult} / ${questions.length} correct`)
+})
