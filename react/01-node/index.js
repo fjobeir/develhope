baf0656b7225262de11f628a6e9696fd5d52a919
Feras Jobeir <fjobeir@gmail.com>
2023-04-18 18:45:43 +0300

    instructions

--- react/01-node/index.js
+++ react/01-node/index.js
@@ -4,3 +4,7 @@ const dayjs = require('dayjs')
 const a = dayjs()
 const b = a.add(7, 'day')
 console.log(b.format("YYYY-MM-DD"))
+
+// download the files
+// make sure to be in the correct path
+// npm install | npm i
